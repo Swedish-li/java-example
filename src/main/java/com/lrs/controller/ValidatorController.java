fb6ffc05b2c8b60820c916a43ecba65e93bf9ad3
Swedish-li <1993770815@qq.com>
2017-02-12 11:02:41 +0800

    spring validator整合测试修改，jdk版本修改

--- src/main/java/com/lrs/controller/ValidatorController.java
+++ src/main/java/com/lrs/controller/ValidatorController.java
@@ -1,25 +1,51 @@
 package com.lrs.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lrs.model.UserVo;
 
 @Controller
 @RequestMapping("valid")
 public class ValidatorController {
+
+	private final static Logger logger = LoggerFactory.getLogger(ValidatorController.class);
+
 	@RequestMapping("valid-user")
+	@ResponseBody
 	public String validUserVo(@Valid @ModelAttribute UserVo user, BindingResult rs, ModelMap model) {
-		
-		if(rs.hasErrors()){
+		logger.info("user:{}", user);
+		logger.info("bind result:{}\nerror:{}", rs, rs.getFieldErrors());
+		List<FieldError> errors = rs.getFieldErrors();
+		for (FieldError error : errors) {
+			String msg = error.getDefaultMessage();
+			String code = error.getCode();
+			logger.error("code:{},msg:{}",code, msg);
+		}
+		logger.info("model map:{}", model);
+		if (rs.hasErrors()) {
 			return "error";
 		}
 		return "page";
 	}
+	/**
+	 * user:com.lrs.model.UserVo@6e5ca6ab[name=li,sex=1] bind
+	 * result:org.springframework.validation.BeanPropertyBindingResult: 0 errors
+	 * model map:{userVo=com.lrs.model.UserVo@6e5ca6ab[name=li,sex=1],
+	 * org.springframework.validation.BindingResult.userVo=org.springframework.validation.BeanPropertyBindingResult:
+	 * 0 errors}
+	 * 
+	 */
 
 }
