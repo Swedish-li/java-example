b883a316a42636c82b399bd92d2895050c20c091
Swedish-li <one.piece.li.2016@gmail.com>
2017-04-13 22:17:01 +0800

    添加Emoji使用示例

--- src/test/java/com/lrs/common/TestEmoji.java
+++ src/test/java/com/lrs/common/TestEmoji.java
@@ -1,12 +1,15 @@
 package com.lrs.common;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Test;
 
 import com.vdurmont.emoji.Emoji;
 import com.vdurmont.emoji.EmojiManager;
 import com.vdurmont.emoji.EmojiParser;
+import com.vdurmont.emoji.EmojiParser.FitzpatrickAction;
 
 /**
  * Emoji(絵文字)
@@ -23,13 +26,66 @@ import com.vdurmont.emoji.EmojiParser;
 public class TestEmoji {
 	@Test
 	public void simple() {
-		// 冒号之间的为颜文字别名
+		// 冒号之间的为Emoji别名
 		String str = "An :grinning:awesome :smiley:string &#128516;with a few :wink:emojis!";
 		// 将颜文字转换为Unicode字符
 		String result = EmojiParser.parseToUnicode(str);
-		System.out.println(result);
+		println(result);
 		// Prints:
 		// "An 😀awesome 😃string 😄with a few 😉emojis!"
+	}
+
+	// 将Emoji的Unicodes形式替换为别名
+	@Test
+	public void replaceUnicode2Aliases() {
+		String string = "An 😀awesome 😃string 😄with a few 😉emojis!";
+		String rs = EmojiParser.parseToAliases(string);
+		// An :grinning:awesome :smiley:string :smile:with a few :wink:emojis!
+		println(rs);
+	}
+
+	@Test
+	public void testFitzpatrick() {
+		String str = "Here is a boy: \uD83D\uDC66\uD83C\uDFFF!";
+		println(str);
+		//parse to Alias
+		println(EmojiParser.parseToAliases(str));
+		println(EmojiParser.parseToAliases(str, FitzpatrickAction.PARSE));
+		
+		println(EmojiParser.parseToAliases(str, FitzpatrickAction.IGNORE));
+		println(EmojiParser.parseToAliases(str, FitzpatrickAction.REMOVE));
+		
+		
+	}
+	// 将Emoji转换为Html的表示方法
+	@Test
+	public void test2Html() {
+		String str = "An 😀awesome 😃string with a few 😉emojis!";
+		// 转换为十进制表示
+		println(EmojiParser.parseToHtmlDecimal(str));
+		
+		// 转换为十六进制表示
+		println(EmojiParser.parseToHtmlHexadecimal(str));
+		
+	}
+	
+	// 将Emoji从字符串中 移除
+	@Test
+	public void testRemoveEmoji() {
+		String str = "An 😀awesome 😃string with a few 😉emojis!";
+		// 移除所有
+		println(EmojiParser.removeAllEmojis(str));
+		// 移除非集合中的Emoji
+		List<Emoji> exceptList = new ArrayList<>();
+		exceptList.add(EmojiManager.getForAlias("wink"));
+		println(EmojiParser.removeAllEmojisExcept(str, exceptList));
+		
+		// 移除集合中的Emoji
+		println(EmojiParser.removeEmojis(str, exceptList));
+	}
+
+	private void println(Object ob) {
+		System.out.println(ob);
 	}
 
 	/**
@@ -44,8 +100,8 @@ public class TestEmoji {
 		// 根据标签获取emoji [letter, p]
 		Collection<Emoji> emojis = EmojiManager.getForTag("p");
 		for (Emoji emoji2 : emojis) {
-			System.out.println(emoji2.getTags());
+			println(emoji2.getTags());
 		}
-		
+
 	}
 }
