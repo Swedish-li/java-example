8a3e385ab6955b5bfee454abd343439f0c96da84
Swedish-li <one.piece.li.2016@gmail.com>
2017-04-23 22:41:32 +0800

    NIO示例添加

---
+++ src/test/java/com/lrs/netty/NioEchoServer.java
@@ -0,0 +1,78 @@
+package com.lrs.netty;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
+public class NioEchoServer {
+	private static final int BUF_SIZE = 256;
+	private static final int TIMEOUT = 3000;
+
+	public static void main(String[] args) throws IOException {
+		// 服务端 Socket
+		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+
+		// 打开Selector
+		Selector selector = Selector.open();
+
+		// 8088端口绑定，设置为非阻塞模式
+		serverSocketChannel.socket().bind(new InetSocketAddress(8088));
+		serverSocketChannel.configureBlocking(false);
+
+		// 将channel注册到 selector
+		serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+		while (true) {
+			// 阻塞等待 channel I/O 可以操作
+			if (selector.select(TIMEOUT) == 0) {
+				System.out.println(".");
+				continue;
+			}
+			// 获取selectionKey的迭代器
+			Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
+
+			while (keyIterator.hasNext()) {
+				
+				SelectionKey key = keyIterator.next();
+
+				if (key.isAcceptable()) {
+					SocketChannel clientChannel = ((ServerSocketChannel) key.channel()).accept();
+					clientChannel.configureBlocking(false);
+					clientChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(BUF_SIZE));
+
+				}
+
+				if (key.isReadable()) {
+					SocketChannel clientChannel = (SocketChannel) key.channel();
+					ByteBuffer buffer = (ByteBuffer) key.attachment();
+					long bytesRead = clientChannel.read(buffer);
+					if (bytesRead == -1) {
+						clientChannel.close();
+					} else if (bytesRead > 0) {
+						key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+						System.out.println("Get data length:" + bytesRead);
+					}
+				}
+
+				if (key.isValid() && key.isWritable()) {
+					ByteBuffer buffer = (ByteBuffer) key.attachment();
+					buffer.flip();
+					SocketChannel clientChannel = (SocketChannel) key.channel();
+					clientChannel.write(buffer);
+					if (!buffer.hasRemaining()) {
+						key.interestOps(SelectionKey.OP_READ);
+					}
+					buffer.compact();
+				}
+				
+				keyIterator.remove();
+			}
+
+		}
+	}
+}
--- src/test/java/com/lrs/netty/NioTest.java
+++ src/test/java/com/lrs/netty/NioTest.java
@@ -5,16 +5,20 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 import org.junit.Test;
-
-import lombok.val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * NIO特性使用示例
+ * 
+ * Java NIO 教程： http://ifeve.com/java-nio-all/
  * 
  * 1、基于Buffer(Buffer Oriented) 2、操作是非阻塞的 3、有selector的概念
  * 
@@ -23,8 +27,61 @@ import lombok.val;
  */
 public class NioTest {
 
+	Logger logger = LoggerFactory.getLogger(NioTest.class);
+
+	// http://ifeve.com/buffers/
+	@Test
+	public void testBuffer() {
+		// 分配10个单位空间
+		IntBuffer buffer = IntBuffer.allocate(10);
+		buffer.put(10);
+		buffer.put(56);
+		logger.info("Buffer: mode:Write,capacity:{},limit:{},position:{}",
+				new Object[] { buffer.capacity(), buffer.limit(), buffer.position() });
+		// 切换为读模式
+		buffer.flip();
+		logger.info("Buffer: mode:Read,capacity:{},limit:{},position:{}",
+				new Object[] { buffer.capacity(), buffer.limit(), buffer.position() });
+	}
+
+	/**
+	 * Buffer
+	 * 
+	 * 1、capacity（容量）: 最多保存的单位数据的数量
+	 * 
+	 * 2、limit(和读写模式有关)：最多可以读取或写入的数据量
+	 * 
+	 * 3、position(和读写模式有关)=>读写操作的位置指针
+	 */
+	@Test
+	public void testIntBuffer() {
+		// 分配2个单位的空间
+		IntBuffer buffer = IntBuffer.allocate(2);
+		// 向Buffer中写入数据
+		buffer.put(12345678);
+		buffer.put(8);
+		// 切换为读模式
+		buffer.flip();
+		// 读取数据
+		// 每当调用一次 get 方法读取数据时, buffer 的读指针都会向前移动一个单位长度(在这里是一个 int 长度)
+		println(buffer.get());
+		println(buffer.get());
+	}
+
+	private void println(Object ob) {
+		System.out.println(ob);
+	}
+
 	/**
 	 * FileChannel
+	 * 
+	 * java中的Nio和流的区别
+	 * 
+	 * 1、既可以从通道中读取数据，又可以向通道中写数据
+	 * 
+	 * 2、通道可以异步读写
+	 * 
+	 * 3、通道总是通过Buffer来读写数据
 	 * 
 	 * @throws IOException
 	 */
@@ -34,15 +91,17 @@ public class NioTest {
 				+ "inc.ftl";
 		RandomAccessFile randomAccessFile = new RandomAccessFile(new File(path), "rw");
 		FileChannel inChannel = randomAccessFile.getChannel();
-		// 分配空间
+		// 分配48个单位的空间
 		ByteBuffer buf = ByteBuffer.allocate(48);
 		// 将channel中的数据读取到 Buffer中
 		int bytesRead = inChannel.read(buf);
 		while (bytesRead != -1) {
+			// 切换buf为读状态
 			buf.flip();
 			while (buf.hasRemaining()) {
 				System.out.print((char) buf.get());
 			}
+			// 转换buf为写模式，也可以使用compact()方法
 			buf.clear();
 			bytesRead = inChannel.read(buf);
 		}
@@ -67,26 +126,42 @@ public class NioTest {
 
 		}
 	}
+
 	@Test
 	public void serverSocketChannelExample() throws IOException {
 		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-		serverSocketChannel.socket().bind(new InetSocketAddress( 9999));
-		
+		serverSocketChannel.socket().bind(new InetSocketAddress(9999));
+
 		while (true) {
-			SocketChannel socketChannel = serverSocketChannel.accept();	
+			SocketChannel socketChannel = serverSocketChannel.accept();
 			ByteBuffer buffer = ByteBuffer.allocate(40);
 			socketChannel.read(buffer);
 			printBuffer(buffer);
 		}
 	}
-		
-	
+
+	/**
+	 * SelectionKey代表一个通道（channel）在 Selector上的注册
+	 */
+
+	@Test
+	public void testSelectionKey() {
+		// SelectionKey.OP_ACCEPT : 1 << 0
+		logger.info("OP_ACCEPT(1 << 0):" + SelectionKey.OP_ACCEPT);
+		logger.info("OP_CONNECT(1 << 2):"+SelectionKey.OP_CONNECT);
+		logger.info("OP_READ(1 << 3):"+SelectionKey.OP_READ);
+		logger.info("OP_WRITE(1 << 4):"+SelectionKey.OP_WRITE);
+	}
+
 	public void printBuffer(ByteBuffer buffer) {
 		// 将Buffer转换为读模式
 		buffer.flip();
-		
+
 		while (buffer.hasRemaining()) {
-			System.out.print(buffer.get());
+			System.out.print((char) buffer.get());
 		}
+		System.out.println();
+		// 将buffer转换为写模式
+		buffer.clear();
 	}
 }
