3a325a7b2dab694871db118c0679b962cf384c8c
Swedish-li <one.piece.li.2016@gmail.com>
2017-10-25 09:28:33 +0800

    'AES，啥'

--- src/test/java/com/lrs/common/auth/EncryptionTech.java
+++ src/test/java/com/lrs/common/auth/EncryptionTech.java
@@ -2,12 +2,23 @@ package com.lrs.common.auth;
 
 import static org.junit.Assert.*;
 
+import static org.hamcrest.Matchers.*;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.Crypt;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.codec.digest.Md5Crypt;
+import org.dom4j.util.SingletonStrategy;
+import org.json.JSONObject;
 import org.junit.Test;
 
 /**
@@ -74,18 +85,10 @@ public class EncryptionTech {
 		for (byte b : cryptBytes) {
 			cryptStrBuilder.append(Integer.toHexString((b & 0xFF) | 0x100));
 		}
-		
-		
-	}
-
-	/**
-	 * 安全散列算法（英语：Secure Hash Algorithm，缩写为SHA）
-	 * 
-	 */
-	@Test
-	public void testSha() {
 
 	}
+
+	
 
 	static final String B64T = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -102,5 +105,84 @@ public class EncryptionTech {
 		}
 		return saltString.toString();
 	}
+	
+	
+	/**
+	 * 安全散列算法（英语：Secure Hash Algorithm，缩写为SHA）
+	 * 
+	 */
 
+	private String signStr = "{\"nickName\":\"Band\","
+			+ "\"gender\":1,\"language\":\"zh_CN\","
+			+ "\"city\":\"Guangzhou\",\"province\":\"Guangdong\","
+			+ "\"country\":\"CN\","
+			+ "\"avatarUrl\":\"http://wx.qlogo.cn/mmopen/vi_32/1vZvI39NWFQ9XM4LtQpFrQJ1xlgZxx3w7bQxKARol6503Iuswjjn6nIGBiaycAjAtpujxyzYsrztuuICqIM5ibXQ/0\"}"
+			+ "HyVFkGl5F5OQWJZZaNzBBg==";
+	
+	/**
+	 * 微信小程序 数据签名校验
+	 */
+	@Test
+	public void testSha1() {
+		String sha1Rs = DigestUtils.sha1Hex(signStr);
+
+		assertThat("75e81ceda165f4ffa64f4068af58c64b8f54b88c", equalTo(sha1Rs));
+	}
+	
+
+	static String AEC_CBC_128 = "AES/CBC/PKCS5Padding";
+
+	String appId = "wx4f4bc4dec97d474b";
+	String sessionKey = "tiihtNczf5v6AKRyjwEUhQ==";
+	String encryptedData = "CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZM" +
+			"QmRzooG2xrDcvSnxIMXFufNstNGTyaGS" +
+			"9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+" +
+			"3hVbJSRgv+4lGOETKUQz6OYStslQ142d" +
+			"NCuabNPGBzlooOmB231qMM85d2/fV6Ch" +
+			"evvXvQP8Hkue1poOFtnEtpyxVLW1zAo6" +
+			"/1Xx1COxFvrc2d7UL/lmHInNlxuacJXw" +
+			"u0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn" +
+			"/Hz7saL8xz+W//FRAUid1OksQaQx4CMs" +
+			"8LOddcQhULW4ucetDf96JcR3g0gfRK4P" +
+			"C7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB" +
+			"6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns" +
+			"/8wR2SiRS7MNACwTyrGvt9ts8p12PKFd" +
+			"lqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYV" +
+			"oKlaRv85IfVunYzO0IKXsyl7JCUjCpoG" +
+			"20f0a04COwfneQAGGwd5oa+T8yO5hzuy" +
+			"Db/XcxxmK01EpqOyuxINew==";
+	String iv = "r7BXXKkLb8qrSNn05n0qiA==";
+
+	String expectedData = "{\"openId\":\"oGZUI0egBJY1zhBYw2KhdUfwVJJE\","
+			+ "\"nickName\":\"Band\",\"gender\":1,\"language\":\"zh_CN\","
+			+ "\"city\":\"Guangzhou\",\"province\":\"Guangdong\",\"country\":\"CN\","
+			+ "\"avatarUrl\":\"http://wx.qlogo.cn/mmopen/vi_32/aSKcBBPpibyKNicHNTMM0qJVh8Kjgiak2AHWr8MHM4WgMEm7GFhsf8OYrySdbvAMvTsw3mo8ibKicsnfN5pRjl1p8HQ/0\","
+			+ "\"unionId\":\"ocMvos6NjeKLIBqg5Mr9QjxrP1FA\","
+			+ "\"watermark\":{\"timestamp\":1477314187,"
+			+ "\"appid\":\"wx4f4bc4dec97d474b\"}}\r\n";
+
+	/**
+	 * 微信小程序AES加密数据解密算法java实现
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testWxappDecryptData() throws Exception {
+		SecretKeySpec skeySpec = new SecretKeySpec(Base64.decodeBase64(sessionKey), "AES");
+
+		IvParameterSpec ivSpec = new IvParameterSpec(Base64.decodeBase64(iv));
+
+		Cipher cipher = Cipher.getInstance(AEC_CBC_128);
+
+		cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivSpec);
+
+		byte[] data = cipher.doFinal(Base64.decodeBase64(encryptedData));
+
+		String decryptedData = new String(data, "utf-8");
+
+		JSONObject object = new JSONObject(decryptedData);
+
+		assertThat(appId, equalTo(object.getJSONObject("watermark").getString("appid")));
+
+	}
 }
